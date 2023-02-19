7ceed38cf1df5c0cfa0afdace030767a058a90a2
SuperNovas <anpugh1@aggies.ncat.edu>
2023-02-19 00:44:09 -0500

    Active Low Change

--- Pump.py
+++ Pump.py
@@ -5,19 +5,20 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 
- # Relay 1
+ # Relay 1 (Active LOW)
 
 GPIO.setup(21, GPIO.OUT)
 
 
 try:
     while True:
-        GPIO.output(21, GPIO.HIGH)
+        GPIO.output(21, GPIO.LOW)
         print('Relay 1 ON')
         time.sleep(5)
-        GPIO.output(21, GPIO.LOW)
+        GPIO.output(21, GPIO.HIGH)
         print('Relay 1 OFF')
         time.sleep(5)
 finally:
-    GPIO.output(21, GPIO.LOW)
+    # GPIO.output(21, GPIO.HIGH)
+    # print('Rela')
     GPIO.cleanup()
