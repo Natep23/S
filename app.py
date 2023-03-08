edea328a8d024f40d11874836b18d6d22c33ff9c
Nathaniel Pugh <nathanielpugh10@gmail.com>
2023-03-08 15:10:02 -0500

    Added Homepage to test on the raspberry pi

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hello World"
+    return render_template("Homepage.html")
 
 
 if __name__ == "__main__":
