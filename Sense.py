3609679a7c64f40db87274ea2053e5d27259d2d1
SuperNovas <anpugh1@aggies.ncat.edu>
2023-03-16 17:27:47 -0400

    differnent code

--- Sense.py
+++ Sense.py
@@ -6,10 +6,10 @@ GPIO.setmode(GPIO.BCM)
 GPIO.setup(4, GPIO.IN)
 
 while True:
-    if(GPIO.input(4) == True):
-        print("Water Detected")
-    else:
+    if(GPIO.input(4) ==True)
         print("No Water Detected")
+    else:
+        print("Water Detected")
 
     time.sleep(2)
 
