c77a6dd778c2397b20c2dd511299318d15cda853
SuperNovas <anpugh1@aggies.ncat.edu>
2023-03-21 15:54:58 -0400

    Updated Sensor Code to Detect from both Sensors

--- Sense.py
+++ Sense.py
@@ -3,13 +3,24 @@ import time
 
 GPIO.setmode(GPIO.BCM)
 
-GPIO.setup(4, GPIO.IN)
+Sensor1 = 4
+Sensor2 = 27
+
+GPIO.setup(Sensor1, GPIO.IN)
+GPIO.setup(Sensor2, GPIO.IN)
+
 
 while True:
-    if(GPIO.input(4) ==True)
-        print("No Water Detected")
+
+    if(GPIO.input(Sensor1) == True):
+        print("Water Detected from Outside Sensor")
     else:
-        print("Water Detected")
+        print("No Water Detected from Outside Sensor")
+
+    if(GPIO.input(Sensor2) == False):
+        print("Water Detected from Inside Sensor")
+    else:
+        print("No Water Detected from Inside Sensor")
 
     time.sleep(2)
 
