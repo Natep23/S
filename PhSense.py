b123b4b500684e5dc3272f37dc39b6eefee6b5c2
SuperNovas <anpugh1@aggies.ncat.edu>
2023-04-15 11:49:13 -0400

    Website Access across multiple devices

--- PhSense.py
+++ PhSense.py
@@ -1,52 +1,25 @@
-#!/usr/bin/python
-''' Raspberry Pi, ADS1115, PH4502C Calibration '''
+import time
 import board
 import busio
-import time
-import sys
-import adafruit_ads1x15.ads1115 as ADS
+import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
-# Setup 
+# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
-ads = ADS.ADS1115(i2c)
 
-def read_voltage(channel):
-    while True:
-        buf = list()
-        
-        for i in range(10): # Take 10 samples
-            buf.append(channel.voltage)
-        buf.sort() # Sort samples and discard highest and lowest
-        buf = buf[2:-2]
-        avg = (sum(map(float,buf))/6) # Get average value from remaining 6
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
 
-        print(round(avg,2),'V')
-        time.sleep(2)
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
+# chan_2 = AnalogIn(ads, ADS.P1)
 
-if __name__ == '__main__':
-    print('\n\n\n')
-    print('---- RPi-ADS115-PH4502 Calibration ----')
-    input('Press Enter once you have grounded the BNC connector...')
-    channel = None
-    while channel not in [0,1,2,3]:
-        try: 
-            channel = int(input('ADS1115 channel 0-3: '))  # ADS.P0, ADS.P1, ADS.P2, ADS.P3
-        except:
-            print('Not a valid option. Try again.')
-    if channel == 0:
-        channel = AnalogIn(ads, ADS.P0)
-    elif channel == 1:
-        channel = AnalogIn(ads, ADS.P1)
-    elif channel == 2:
-        channel = AnalogIn(ads, ADS.P1)
-    elif channel == 3:
-        channel = AnalogIn(ads, ADS.P1)
-    else:
-        sys.exit('Error selecting an ADS1115 pin.')
-    print('Adjust potentiometer nearest to BNC socket to ~2.50V')
-    print('Starting readings. Press CTRL+C to stop...')
-    try:
-        read_voltage(channel)
-    except KeyboardInterrupt:
-        pass
+# Create differential input between channel 0 and 1
+# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+print("------{:>5}\t{:>5}".format("raw", "v"))
+
+while True:
+    print("CHAN 0: "+"{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+    # print("CHAN 1: "+"{:>5}\t{:>5.3f}".format(chan_2.value, chan_2.voltage))
+    time.sleep(0.5)
--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template, redirect, request
+import socket
 # import Pump.py
 # import Sense.py
+
+# Grabs the Local IP address to allow the Rasberry Pi Run the Website on Multiple Devices on the same wifi network
+def getRaspIp():
+   ip = ''
+   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+   s.connect(("8.8.8.8", 80))
+   ip = s.getsockname() [0]
+   s.close()
+   return ip
 
 app = Flask(__name__)
 
@@ -10,4 +20,4 @@ def index():
 
 
 if __name__ == "__main__":
-    app.run(host="192.168.1.238", port=5000, debug=True) 
+    app.run(host=getRaspIp(), port=5000, debug=True) 
